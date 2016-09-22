7b7641e75e960fcd2ee5212aaae223d843007684
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2016-09-22 10:40:46 +0200

    Missing BLE methods

--- js/cordova/plugin/BluetoothLE.hx
+++ js/cordova/plugin/BluetoothLE.hx
@@ -1,5 +1,17 @@
 package js.cordova.plugin;
 
+@:enum
+abstract BTState(String) from String to String {
+    var On = "on";
+    var Off = "off";
+    var TurningOn = "turningOn"; // (Android Only)
+    var TurningOff = "turningOff"; // (Android Only)
+    var Unknown = "unknown"; // (iOS Only)
+    var Resetting = "resetting"; // (iOS Only)
+    var Unsupported = "unsupported"; // (iOS Only)
+    var Unauthorized = "unauthorized"; // (iOS Only)
+}
+
 typedef BluetoothLEPeripheral = {
 	name : String,
     id : String,
@@ -31,6 +43,8 @@ extern class BluetoothLE {
 	public static function disconnect(id : String, onSuccess : Void -> Void, onError : String -> Void) : Void;
 	public static function write(deviceId : String, serviceUuid : String, characteristicUuid : String, data : js.html.ArrayBuffer, onSuccess : Dynamic -> Void, onError : String -> Void) : Void;
 	public static function writeWithoutResponse(deviceId : String, serviceUuid : String, characteristicUuid : String, data : js.html.ArrayBuffer, onSuccess : Dynamic -> Void, onError : String -> Void) : Void;
+    public static function startStateNotifications(success : BTState -> Void, failure : String -> Void) : Void;
+    public static function stopStateNotifications(success : Void -> Void, failure : String -> Void) : Void;
 	public static function startNotification(deviceId : String, serviceUuid : String, characteristicUuid : String, success : js.html.ArrayBuffer -> Void, failure : String -> Void) : Void;
 	public static function read(deviceId : String, serviceUuid : String, characteristicUuid : String, success : js.html.ArrayBuffer -> Void, failure : String -> Void) : Void;
     public static function stopNotification(deviceId : String, serviceUuid : String, characteristicUuid : String, ? success : Void -> Void, ? failure : String -> Void) : Void;
